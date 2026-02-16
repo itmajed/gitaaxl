d5bca809f776742062c7986fe43322a498701c82
itmajed <itmajedai@gmail.com>
2026-02-16 06:41:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env.API_KEY': JSON.stringify('AIzaSyAnOLW8dAQa6WKnSLHLC1dCO9MmnyAVyfI'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './'),
-      },
-    },
-    build: {
-      outDir: 'dist',
-    }
-  };
+  },
+  build: {
+    outDir: 'dist',
+  }
 });
