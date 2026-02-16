793acbf35433160a3fdbcfcfe18692641c28c601
itmajed <itmajedai@gmail.com>
2026-02-16 03:31:42 +0300

    Update Vite config to simplify server settings
    Removed path alias configuration and build output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-    },
-  },
-  build: {
-    outDir: 'dist',
+  server: {
+    host: true
   }
 });
